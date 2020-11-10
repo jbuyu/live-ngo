029d0eb00a4d8507b2cf2a34261850e49125bf86
jbuyu <jbuyu@ymail.com>
2020-11-10 18:55:19 +0300

    removes footer, edits page

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,11 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Ngo reel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div>Main Ngo section</div>
     </div>
   );
 }
