4edd6256a1661a08c30b786280c5b5f654f03df1
jbuyu <jbuyu@ymail.com>
2020-11-10 19:51:01 +0300

    sets up navbar links, adds react-icons

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Head from "next/head";
+import Footer from "../src/components/Footer";
 import Navbar from "../src/components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Ngo reel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
 
-      <div>Main Ngo section</div>
-    </div>
+      <div className={styles.container}>
+        <div>Main Ngo section</div>
+      </div>
+      <Footer />
+    </>
   );
 }
