7d9e7f3f26f38b5df21e1b773a6da6296c6adcab
jbuyu <jbuyu@ymail.com>
2020-12-14 11:16:06 +0300

    fixes on strategies css

--- components/TimelineItem.js
+++ components/TimelineItem.js
@@ -2,9 +2,9 @@ const TimelineItem = ({ data }) => (
   <div className="timeline-item">
     <div className="timeline-item-content">
       <span className="tag" style={{ background: data.category.color }}>
-        {data.category.tag}
+        <div className="tag-content">{data.category.tag}</div>
       </span>
-      <p className="component">{data.component}</p>
+      {/* <p className="component">{data.component}</p> */}
       <p className="text">{data.text}</p>
       <span className="circle"></span>
     </div>
