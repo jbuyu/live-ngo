2f4a7289850a4e5f4b831d09ad2169694414ea16
jbuyu <jbuyu@ymail.com>
2021-01-05 18:13:25 +0300

    adds patner section

--- components/Patners.js
+++ components/Patners.js
@@ -1,14 +1,20 @@
+import Patlogo1 from "../public/patlogo1.svg";
+import Patlogo2 from "../public/patlogo2.svg";
+import Patlogo3 from "../public/patlogo3.svg";
+import Patlogo4 from "../public/patlogo4.svg";
 
 export default function Patners() {
   return (
     <div className="patners">
-        <div className="meet-patners">
-            our <span>Patners</span>
-        </div>
-        <div className="patner-logos">
-            
-        </div>
-      
+      <div className="meet-patners">
+        our <span>Patners</span>
+      </div>
+      <div className="patner-logos">
+        <img src={Patlogo1} alt="patner_logo" width={130} />
+        <img src={Patlogo2} alt="patner_logo" width={130} />
+        <img src={Patlogo3} alt="patner_logo" width={130} />
+        <img src={Patlogo4} alt="patner_logo" width={130} />
+      </div>
     </div>
   );
 }
