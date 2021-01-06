7db62c10e50b543683f8314e76102a9b41369aaa
jbuyu <jbuyu@ymail.com>
2021-01-06 14:19:03 +0300

    css for solution

--- pages/solution.js
+++ pages/solution.js
@@ -1,39 +1,27 @@
-import Image from 'next/image'
+import Image from "next/image";
 export default function Solution() {
   return (
     <section className="solution">
-      <div className="solution-bg"></div>
-      <div className="container">
-        <div className="row">
-          <div className="solution-inner" id="solutionInner">
-            <div className="content">
-              <div className="content-inner">
-                <h1>
-                  <div className="solution-content">
-                    <div className="content-paragraph">
-                      <p>
-                        BoldStep Initiative, through its programs, broadly
-                        addresses gaps in business skills and competency for the
-                        Kenya youth, focusing on issues that include critical
-                        thinking necessary for generating initiative and
-                        innovation, problem solving and decision making. The
-                        programs emphasize on the application of principles of
-                        transferable skills in a competency based learning
-                        approach. We work with Kenyan youth aged{" "}
-                        <strong>18-29 </strong> years old, who are recent{" "}
-                        <strong>college / university</strong> graduates, young
-                        professionals and entrepreneurs to lay the foundation
-                        for their posperity.
-                      </p>
-                    </div>
-                    <div className="image">
-
-                    <Image src="/solution.svg" alt="solutions svg" width="900" height="900" />
-                    </div>
-                  </div>
-                </h1>
-              </div>
-            </div>
+      <div className="solution-inner">
+        <div className="solution-content">
+          <p>
+            BoldStep Initiative, through its programs, broadly addresses gaps in
+            business skills and competency for the Kenya youth, focusing on
+            issues that include critical thinking necessary for generating
+            initiative and innovation, problem solving and decision making. The
+            programs emphasize on the application of principles of transferable
+            skills in a competency based learning approach. We work with Kenyan
+            youth aged <strong>18-29 </strong> years old, who are recent
+            <strong>college / university</strong> graduates, young professionals
+            and entrepreneurs to lay the foundation for their posperity.
+          </p>
+          <div className="image">
+            <Image
+              src="/solution.svg"
+              alt="solutions svg"
+              width="900"
+              height="900"
+            />
           </div>
         </div>
       </div>
